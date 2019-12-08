20a0fbcd4007cb6256f796d943ec128bfcb06c8a
Doosung Jang <teurowel@gmail.com>
2019-12-07 20:59:36 -0500

    Working on cylinder

--- FirstExample/Cylinder.cpp
+++ FirstExample/Cylinder.cpp
@@ -1,5 +1,7 @@
 #include "Cylinder.h"
 
+#include <cmath>
+
 Cylinder::Cylinder()
 {
 
@@ -12,35 +14,65 @@ Cylinder::~Cylinder()
 
 void Cylinder::CreateMesh()
 {
-	//m_vertexCount = (iNumOfGrid + 1) * (iNumOfGrid + 1);
+	const float PI = 3.1415926f;
+	float sectorCount = 10.f;
+	float sectorStep = 2 * PI / sectorCount;
+	float sectorAngle = 0.f;  // radian
 
-	//GLfloat* plane_vertices = new GLfloat[m_vertexCount * m_vertexLength];
-	//int iIndex = 0;
-	//for (int j = 0; j <= iNumOfGrid; ++j)
-	//{
-	//	for (int i = 0; i <= iNumOfGrid; ++i)
-	//	{
-	//		float x = (float)i / (float)iNumOfGrid;
-	//		float y = 0;
-	//		float z = (float)j / (float)iNumOfGrid;
+	//Top circle + center of circle
+	m_vertexCount = sectorCount + 1;
+	GLfloat* vertices = new GLfloat[m_vertexCount * m_vertexLength];
+	int iIndex = 0;
 
-	//		//x, y, z
-	//		plane_vertices[iIndex] = x;
-	//		plane_vertices[iIndex + 1] = y;
-	//		plane_vertices[iIndex + 2] = z;
+	//Center of circle
+	vertices[iIndex] = 0.f;
+	vertices[iIndex + 1] = 0.5f;
+	vertices[iIndex + 2] = 0.f;
 
-	//		//u, v
-	//		plane_vertices[iIndex + 3] = (float)i / (float)iNumOfGrid;
-	//		plane_vertices[iIndex + 4] = (float)j / (float)iNumOfGrid;
+	//u, v
+	vertices[iIndex + 3] = 0.5f;
+	vertices[iIndex + 4] = 0.5f;
 
-	//		//Normal
-	//		plane_vertices[iIndex + 5] = 0.f;
-	//		plane_vertices[iIndex + 6] = 0.f;
-	//		plane_vertices[iIndex + 7] = 0.f;
+	//Normal
+	vertices[iIndex + 5] = 0.f;
+	vertices[iIndex + 6] = 1.f;
+	vertices[iIndex + 7] = 0.f;
 
-	//		iIndex += m_vertexLength;
-	//	}
-	//}
+	iIndex += m_vertexLength;
+	for (int i = 0; i < sectorCount; ++i)
+	{
+		sectorAngle = i * sectorStep;
+
+		//Center of circle
+		vertices[iIndex] = cos(sectorAngle);
+		vertices[iIndex + 1] = 0.5f;
+		vertices[iIndex + 2] = sin(sectorAngle);
+
+		//u, v
+		vertices[iIndex + 3] = cos(sectorAngle) * 0.5f + 0.5f;
+		vertices[iIndex + 4] = sin(sectorAngle) * 0.5f + 0.5f;
+
+		//Normal
+		vertices[iIndex + 5] = 0.f;
+		vertices[iIndex + 6] = 1.f;
+		vertices[iIndex + 7] = 0.f;
+
+		iIndex += m_vertexLength;
+	}
+
+	m_indexCount = sectorCount * 3;
+	GLshort* indices = new GLshort[m_indexCount];
+	iIndex = 0;
+	for (int i = 1; i < sectorCount + 1; ++i)
+	{
+		indices[iIndex] = i;
+		indices[iIndex + 1] = 0;
+		if (i == sectorCount)
+			indices[iIndex + 2] = 1;
+		else
+			indices[iIndex + 2] = i + 1;
+		iIndex += 3;
+	}
 
 	////Create Index
 	//m_indexCount = (iNumOfGrid * iNumOfGrid) * 2 * 3;
@@ -71,8 +103,8 @@ void Cylinder::CreateMesh()
 
 	//Mesh::calcAverageNormals(plane_indices, m_indexCount, plane_vertices, m_vertexCount, m_vertexLength, 5);
 
-	//Mesh::CreateMesh(plane_vertices, plane_indices, m_vertexCount, m_indexCount);
+	Mesh::CreateMesh(vertices, indices, m_vertexCount, m_indexCount);
 
-	//delete[] plane_indices;
-	//delete[] plane_vertices;
+	delete[] indices;
+	delete[] vertices;
 }
